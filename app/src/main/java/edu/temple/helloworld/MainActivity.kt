0211caf1dc4a67e79c0aa5ee1cd4f5cdda1975bc
zxciop753 <z568216219@gmail.com>
2021-09-15 11:49:03 -0400

    Add Button

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         findViewById<Button>(R.id.button)
             .setOnClickListener({v, -> (v as Button).text = "You clicked the button"})
 
